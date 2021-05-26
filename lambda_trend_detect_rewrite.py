8071ab1c976b5696cdeaf4f2114939729344a03f
keithyamkf <keithyamkf@penguin>
2021-05-26 22:27:52 +0800

    rewrite the lambda_trend_detect to use common module-fix1

--- lambda_trend_detect_rewrite.py
+++ lambda_trend_detect_rewrite.py
@@ -20,7 +20,7 @@ def lambda_handler(event, handler):
 	for msg in event['Records']:
 		L1 = msg['body']
 		load_csv_run_analysis(L1)
-'''
+
 event = {'Records': [
             {'messageId': '67df3bef-db40-4efd-8ab3-9a91ac2a8bef',
             'receiptHandle': 'AQICb/KtCVgWXxCZhbDcNki/rtsKyag6EE2gA08aer0eAknp3ceEsiW9hQKxQHuIWps8VPiqmR5+A7xlOo+3etrXGJZ4x1dcIQRYXyeoHq/sdlvw1K4AoAGGEVwAgMjBb3+WR6N3yrDTt4OCOc4lO7SuUyTe1IoFiiSsSzp9T4xUIWmH3FmLDK2QmIdzi8t7GFqbSZ9m46WkunBmSDiAxkc+NMBseygCWcW3a5+SoSYk5hfJsvASzyU5WQBu3hrEubu77sCVALO9vKWvSC4uKp5cThzdSVDXyggf4H3Z8rT8WweSkTFtxiqwRWFkBnqbdS2l3EsNXte6QkKFe1tWmxe3krdi+L6RLCfKrtgMcgRSWqHvq5t8ro8e03Kn3qFJt5wUsm2K6HqeWIN1gwfmyHqLCg==', 
@@ -39,4 +39,3 @@ event = {'Records': [
             ]
         }
 lambda_handler(event, None)
-'''
